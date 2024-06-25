5d604b80627bda310f98db869103bdc9976fdf8f
Tom Carman <tom.carman@gmail.com>
2024-06-25 23:01:26 +0100

    refactor: refactor rule picklist-values-should-not-contain-double-spaces

--- src/rules/picklist-values-should-not-contain-double-spaces.ts
+++ src/rules/picklist-values-should-not-contain-double-spaces.ts
@@ -6,7 +6,7 @@ import { parseMetadataXml, getLineAndColNumber } from '../commands/util.js';
 export default class PicklistValuesShouldNotContainDoubleSpaces extends RuleClass {
   public ruleId: string = 'picklist-values-should-not-contain-double-spaces';
   public shortDescriptionText = 'Picklist values should not contain double spaces.';
-  public fullDescriptionText = "Picklist values should not contain double spaces eg. 'Status  Pending'.";
+  public fullDescriptionText = "Picklist values should not contain double spaces eg. 'Offer  Made'.";
   public startLine = 1;
   public endLine = 1;
 
@@ -15,25 +15,27 @@ export default class PicklistValuesShouldNotContainDoubleSpaces extends RuleClas
       (file) => (file.includes('__c') || file.includes('__e')) && file.endsWith('.field-meta.xml')
     );
 
-    for (const file of customFields) {
+    customFields.forEach((file) => {
       const fileText = fs.readFileSync(file, 'utf-8');
       const customField = parseMetadataXml<CustomField>(fileText, 'CustomField');
-      if (customField.type === 'Picklist' || customField.type === 'MultiselectPicklist') {
-        if (customField.valueSet?.valueSetDefinition) {
-          // Picklist field has inline valueset
-          for (const value of customField.valueSet.valueSetDefinition.value) {
-            if (value.label?.includes('  ')) {
-              const [lineNumber, colNumber] = getLineAndColNumber(fileText, value.label);
-              this.startLine = this.endLine = lineNumber;
-              this.results.push(new SingleRuleResult(file, this.startLine, this.endLine, colNumber, colNumber));
-            }
-            if (value.fullName?.includes('  ')) {
-              const [lineNumber, colNumber] = getLineAndColNumber(fileText, value.fullName);
-              this.startLine = this.endLine = lineNumber;
-              this.results.push(new SingleRuleResult(file, this.startLine, this.endLine, colNumber, colNumber));
-            }
-          }
-        }
+      if (customField.type && ['Picklist', 'MultiselectPicklist'].includes(customField.type)) {
+        customField.valueSet?.valueSetDefinition?.value.forEach((value) => {
+          checkForDoubleSpace.call(this, file, fileText, value.label);
+          checkForDoubleSpace.call(this, file, fileText, value.fullName);
+        });
+      }
+    });
+
+    function checkForDoubleSpace(
+      this: PicklistValuesShouldNotContainDoubleSpaces,
+      file: string,
+      fileText: string,
+      value?: string
+    ): void {
+      if (value?.includes('  ')) {
+        const [lineNumber, colNumber] = getLineAndColNumber(fileText, value);
+        this.startLine = this.endLine = lineNumber;
+        this.results.push(new SingleRuleResult(file, this.startLine, this.endLine, colNumber, colNumber));
       }
     }
   }
