f8b2f7013299db1f2859f5d2aa3c0e25ab753d5f
Tom Carman <tom.carman@gmail.com>
2024-06-29 00:50:04 +0100

    fix: move to experimental/

--- src/rules/no-dml-in-flow-for-loop.ts
+++ src/rules/no-dml-in-flow-for-loop.ts
@@ -2,7 +2,7 @@ import * as fs from 'node:fs';
 import type { Flow } from '@salesforce/types/metadata';
 import { RuleClass } from '../common/types.js';
 import { parseMetadataXml } from '../common/util.js';
-import { FlowWrapper } from '../common/FlowWrapper.js';
+import { FlowWrapper } from '../common/experimental/FlowWrapper.js';
 
 export default class NoDmlInFlowForLoop extends RuleClass {
   public ruleId: string = 'no-missing-description-on-fields';
@@ -31,3 +31,61 @@ export default class NoDmlInFlowForLoop extends RuleClass {
     }
   }
 }
+
+// Flow: Sample PMD flow
+
+// - Node: cascadeCity1
+//    Connector Type: connector
+//        Connects to :badUpdate1
+
+// - Node: cascadeCity2
+//    Connector Type: connector
+//        Connects to :goodLoop1
+
+// - Node: call_Apex_code
+//    Connector Type: connector
+//        Connects to :suspiciousLoop1
+
+// - Node: getContacts
+//    Connector Type: connector
+//        Connects to :whichCity
+
+// - Node: badUpdate1
+//    Connector Type: connector
+//        Connects to :badLoop1
+
+// - Node: goodUpdate1
+//    Connector Type: connector
+//        Connects to :X1_more_update
+
+// - Node: X1_more_update
+
+// - Node: whichCity
+//    Connector Type: defaultConnector
+//        Connects to :suspiciousLoop1
+//    Connector Type: connector
+//        Connects to :badLoop1
+//    Connector Type: connector
+//        Connects to :goodLoop1
+
+// - Node: badLoop1
+//    Connector Type: nextValueConnector
+//        Connects to :cascadeCity1
+//    Connector Type: noMoreValuesConnector
+//        Connects to :X1_more_update
+
+// - Node: goodLoop1
+//    Connector Type: nextValueConnector
+//        Connects to :cascadeCity2
+//    Connector Type: noMoreValuesConnector
+//        Connects to :goodUpdate1
+
+// - Node: suspiciousLoop1
+//    Connector Type: nextValueConnector
+//        Connects to :call_Apex_code
+//    Connector Type: noMoreValuesConnector
+//        Connects to :X1_more_update
+
+// - Node:
+//    Connector Type: connector
+//        Connects to :getContacts
